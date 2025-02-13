75576ede050ea54b873d0a7abf40ac64154f35af
Silverplata <jplata@utedevas.es>
2025-02-14 00:17:06 +0100

    Creamos un componente Blog
    Creamos tambien la interfaz IBlog

---
+++ src/app/components/blog/blog.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { IBlog } from '../../interfaces/iblog.interface';
+
+@Component({
+  selector: 'app-blog',
+  imports: [],
+  templateUrl: './blog.component.html',
+  styleUrl: './blog.component.css'
+})
+export class BlogComponent {
+
+}
