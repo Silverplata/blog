27c2ef110b132596697b067a62e89cf217f58154
Silverplata <jplata@utedevas.es>
2025-02-14 01:03:39 +0100

    Creamos el formulario y su maquetacion
    Creamos array e inicializamos dos noticias

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { BlogComponent } from "./components/blog/blog.component";
 
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [BlogComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
